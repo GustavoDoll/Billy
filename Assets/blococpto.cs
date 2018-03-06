9cbb3fed2a14cab3f1821e23d021dbc1d3f6d9db
GustavoDoll <gustavohdoll@hotmail.com>
2018-03-06 14:32:30 -0300

    Initial commit

---
+++ Assets/blococpto.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blococpto : MonoBehaviour {
+	bool chave1;
+	bool chave2;
+	bool chave3;
+	bool chaveSom;
+	bool chave;
+	bool key1;
+	bool key2;
+	bool key3;
+	public GameObject Elefante;
+	public GameObject Tela2;
+	public GameObject tela1;
+	public AudioSource lvlpass;
+	public AudioSource lvlcompleted;
+	public GameObject TextoFinal2;
+	public GameObject TextoFinal1;
+
+
+
+
+	void OnCollisionStay(Collision c){
+		
+			 if (c.collider.tag == "quadCol1") {
+				chave = true;
+				
+			} else if (c.collider.tag == "quadCOl2" && chave == true) {
+
+				chave2 = true;
+				
+			} else if (c.collider.tag == "quadCol3" && chave2 == true) {
+
+				chave3 = true;
+				
+			} 
+		else if (c.collider.tag == "quadCol4" && chave3 == true) {
+			lvlpass.Play ();	
+			Tela2.SetActive (true);
+			tocaanimacao.tempo += 10.0f;
+			tela1.SetActive (false);
+
+		} 
+	
+
+		if (c.collider.tag == "triCol1") {
+			key1 = true;
+		
+		}
+		else if (c.collider.tag=="triCol2" && key1 == true) {
+			key2 = true;
+
+		}
+		else if (c.collider.tag=="triCol3" && key2 == true) {
+			key3 = true;
+
+		}
+		else if (c.collider.tag=="triCol4" && key3 == true) {
+			lvlcompleted.Play ();
+			Tela2.SetActive (false);
+			TextoFinal1.SetActive (true);
+			TextoFinal2.SetActive (true);
+			Elefante.GetComponent<Animator>().Play("Move");
+
+		}
+}
+}
---
+++ Assets/tocaanimacao.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tocaanimacao : MonoBehaviour {
+	public Animator animacao;
+	public GameObject objeto;
+	public GameObject balao;
+	public GameObject Tela1;
+	public GameObject texto;
+	public GameObject texto2;
+	public GameObject tela2;
+	public AudioSource lvlpass;
+	public Transform hud;
+	public Text txtTempo;
+	public AudioSource fala;
+	public AudioSource somElefante;
+	Vector3 posicao;
+	public AudioSource MusicadeFundo;
+	public static float tempo;
+
+	bool validacao = false;
+	bool chave = true;
+	// Use this for initialization
+	void Start () {
+		tempo = 60.0f;
+		MusicadeFundo.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonUp (0) && validacao == false && chave ==  true) {
+			animacao.Play ("Idle");
+			texto.SetActive (true);
+			texto2.SetActive (true);
+			fala.Play ();
+			somElefante.Stop ();
+			Destroy (balao);
+			validacao = true;
+		} 
+		if (Input.GetMouseButtonDown(0) && validacao == true && chave == true) {
+			texto.SetActive (false);
+			texto2.SetActive (false);
+			objeto.SetActive (true);
+
+			hud.gameObject.SetActive (true);
+			StartCoroutine (ContadorDeTempo ());
+			Tela1.SetActive (true);
+			chave = false;
+		}
+
+
+		SegueToque ();
+
+		txtTempo.text = "Tempo:" + tempo.ToString ();
+
+
+		if (tempo == 0) {
+			Application.LoadLevel ("Teste");
+		}
+	}
+		
+	void SegueToque(){
+
+		Ray ray;
+		RaycastHit hit;
+
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
+
+		// Coordenada do toque
+		if(Input.GetButton("Fire1")){
+			if(Physics.Raycast(ray, out hit)){
+				// Coordenada do que no plano 3D
+				posicao = new Vector3(hit.point.x, hit.point.y, 0.0f);
+			}
+		}
+
+		// Move o objeto
+		objeto.transform.position = Vector3.Lerp(objeto.transform.position, posicao, 7.0f * Time.deltaTime);
+
+	}
+
+	IEnumerator ContadorDeTempo(){
+		yield return new WaitForSeconds (1.0f);
+		tempo--;
+		StartCoroutine (ContadorDeTempo ());
+
+	}
+}
